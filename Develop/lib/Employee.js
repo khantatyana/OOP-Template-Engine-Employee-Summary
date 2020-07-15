c845e7f5b42855f80fc65a3d4b61f824517b7e57
Tatyana <hanyatan@gmail.com>
2020-07-15 17:04:36 -0400

    added classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    // Just like constructor functions, classes can accept arguments
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email
+    }
+    getName = () => {
+
+    }
+    getId = () => {
+
+    }
+    getEmail = () => {
+
+    }
+    getRole = () => {
+        // return "Employee"
+    }
+}
+// Employee.prototype.toString() = function() {
+
+// }
+  
+module.exports = Employee;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(officeNumber) {
+    getRole = () => {
+        // return "Manager"
+    }
+
+    super(name, id, email);
+    this.officeNumber = officeNumber
+  }
+
+}
+module.exports = Manager;
+
